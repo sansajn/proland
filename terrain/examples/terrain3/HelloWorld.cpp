bdfa6f8fb148b4f50dd325c5255c8cc1ac3ae054
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2024-05-15 15:27:28 +0200

    working terrain 3 example

--- terrain/examples/terrain3/HelloWorld.cpp
+++ terrain/examples/terrain3/HelloWorld.cpp
@@ -30,6 +30,7 @@
 #include "ork/core/FileLogger.h"
 #include "ork/render/FrameBuffer.h"
 #include "ork/resource/XMLResourceLoader.h"
+#include "ork/resource/InitResources.h"
 #include "ork/scenegraph/SceneManager.h"
 #include "ork/scenegraph/ShowLogTask.h"
 #include "ork/ui/GlutWindow.h"
@@ -212,6 +213,7 @@ static_ptr<Window> HelloWorld::app;
 
 int main(int argc, char* argv[])
 {
+    ork::InitResourceTypes();
     initTerrainPlugin();
     atexit(HelloWorld::exit);
     HelloWorld::app = new HelloWorld();
